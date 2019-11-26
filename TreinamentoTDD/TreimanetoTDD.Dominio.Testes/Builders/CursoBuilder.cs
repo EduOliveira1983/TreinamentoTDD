7b3b55087cdb380632dd263922ed03eecc67110e
Eduardo Oliveira <eduardooliveira_jr@hotmail.com>
2019-11-26 13:50:53 -0300

    Continuação do Curso

---
+++ TreinamentoTDD/TreimanetoTDD.Dominio.Testes/Builders/CursoBuilder.cs
@@ -0,0 +1,65 @@
+﻿using TreimanetoTDD.Dominio.Testes.Cursos;
+
+namespace TreimanetoTDD.Dominio.Testes.Builders
+{
+    /// <summary>
+    /// Builder nada mais é do que um construtor do objeto, dessa forma, concentro toda a construção em um unico lugar    /// 
+    /// </summary>
+    public class CursoBuilder
+    {
+        #region Variaveis Internas
+        private  string _nome = "Curso Teste";
+        private  int _cargaHoraria = 40;
+        private  PublicoAlvo _publicoAlvo = PublicoAlvo.Estudante;
+        private  decimal _valor = 4000;
+        private  string _descricao = "Descrição";
+        #endregion
+
+        public static CursoBuilder Novo()
+        {
+            return new CursoBuilder();
+        }
+
+        public Curso Build()
+        {
+            return new Curso(_nome, _cargaHoraria, _publicoAlvo, _valor, _descricao);
+        }
+
+        public CursoBuilder ComNome(string nome)
+        {
+            _nome = nome;
+            return this;
+        }
+        public CursoBuilder ComDescricao(string descricao)
+        {
+            _descricao = descricao;
+            return this;
+        }
+
+        public CursoBuilder ComCargaHoraria(int cargaHoraria)
+        {
+            _cargaHoraria = cargaHoraria;
+            return this;
+        }
+
+        public CursoBuilder ComPublicoAlvo(PublicoAlvo publicoAlvo)
+        {
+            _publicoAlvo = publicoAlvo;
+            return this;
+        }
+
+        public CursoBuilder ComValor(decimal valor)
+        {
+            _valor = valor;
+            return this;
+        }
+
+
+
+
+
+
+
+
+    }
+}
--- TreinamentoTDD/TreimanetoTDD.Dominio.Testes/Cursos/CursoTestes.cs
+++ TreinamentoTDD/TreimanetoTDD.Dominio.Testes/Cursos/CursoTestes.cs
@@ -1,12 +1,13 @@
 ﻿using ExpectedObjects;
 using System;
 using TreimanetoTDD.Dominio.Testes._Util;
+using TreimanetoTDD.Dominio.Testes.Builders;
 using Xunit;
 
 namespace TreimanetoTDD.Dominio.Testes.Cursos
 {
     public class CursoTestes
-    {
+    {   
         [Fact]
         public void DeveCriarCurso()
         {
@@ -15,74 +16,54 @@ namespace TreimanetoTDD.Dominio.Testes.Cursos
                 Nome = "Curso Teste",
                 CargaHoraria = (int)40,
                 PublicoAlvo = PublicoAlvo.Estudante,
-                Valor = (decimal)4000
+                Valor = (decimal)4000,
+                Descricao = "Descricao Teste"
             };
 
             var curso = new Curso(cursoEsperado.Nome,
                                   cursoEsperado.CargaHoraria,
                                   cursoEsperado.PublicoAlvo,
-                                  cursoEsperado.Valor);
+                                  cursoEsperado.Valor,
+                                  cursoEsperado.Descricao );
 
             cursoEsperado.ToExpectedObject().ShouldMatch(curso);
         }
+
 
         [Theory]
         [InlineData("")]
         [InlineData(null)]
         public void NaoDeveCursoTerUmNomeInvalido(string nome)
-        {
-            var cursoEsperado = new
-            {
-                Nome = nome,
-                CargaHoraria = (int)40,
-                PublicoAlvo = PublicoAlvo.Estudante,
-                Valor = (decimal)4000
-            };
-
-            Assert.Throws<ArgumentException>(() => new Curso(cursoEsperado.Nome,
-                                                             cursoEsperado.CargaHoraria,
-                                                             cursoEsperado.PublicoAlvo,
-                                                             cursoEsperado.Valor)).ComMensagem("Nome Inválido");
+        { 
+            Assert.Throws<ArgumentException>(() => CursoBuilder.Novo().ComNome(nome).Build())
+                .ComMensagem("Nome Inválido");
         }
 
         [Theory]
         [InlineData(0)]
         [InlineData(-1)]
         public void NaoDeveCurtoTerCargaHorariaMenorqueHum(int cargaHoraria)
-        {
-            var cursoEsperado = new
-            {
-                Nome = "Curso Teste",
-                CargaHoraria = cargaHoraria,
-                PublicoAlvo = PublicoAlvo.Estudante,
-                Valor = (decimal)4000
-            };
-
-            Assert.Throws<ArgumentException>(() => new Curso(cursoEsperado.Nome,
-                                                             cursoEsperado.CargaHoraria,
-                                                             cursoEsperado.PublicoAlvo,
-                                                             cursoEsperado.Valor)).ComMensagem("Carga Horária Inválida");
-
+        {           
+            Assert.Throws<ArgumentException>(() => CursoBuilder.Novo().ComCargaHoraria(cargaHoraria).Build())
+                .ComMensagem("Carga Horária Inválida");
         }
 
         [Theory]
         [InlineData(-1)]
-        public void NaoDeveCurtoTerValorMenorQueZero(decimal Valor)
+        public void NaoDeveCurtoTerValorMenorQueZero(decimal valor)
         {
-            var cursoEsperado = new
-            {
-                Nome = "Curso Teste",
-                CargaHoraria = (int)40,
-                PublicoAlvo = PublicoAlvo.Estudante,
-                Valor = Valor
-            };
+            Assert.Throws<ArgumentException>(() => CursoBuilder.Novo().ComValor(valor).Build())
+                .ComMensagem("Valor Inválido");
+        }
 
-            Assert.Throws<ArgumentException>(() => new Curso(cursoEsperado.Nome,
-                                                             cursoEsperado.CargaHoraria,
-                                                             cursoEsperado.PublicoAlvo,
-                                                             cursoEsperado.Valor)).ComMensagem("Valor Inválido");
+        [Fact]
+        public void NaoDeveCursoTerDescricaoInvalida()
+        {
+            Assert.Throws<ArgumentException>(() => CursoBuilder.Novo().ComDescricao(null).Build())
+               .ComMensagem("Descrição Inválida");
         }
     }
+        
 
     public enum PublicoAlvo
     {
@@ -94,10 +75,13 @@ namespace TreimanetoTDD.Dominio.Testes.Cursos
 
     public class Curso
     {
-        public Curso(string nome, int cargaHoraria, PublicoAlvo publicoAlvo, decimal valor)
+        public Curso(string nome, int cargaHoraria, PublicoAlvo publicoAlvo, decimal valor, string descricao)
         {
             if (string.IsNullOrEmpty(nome))
                 throw new ArgumentException("Nome Inválido");
+
+            if (string.IsNullOrEmpty(descricao))
+                throw new ArgumentException("Descrição Inválida");
 
             if (cargaHoraria < 1)
                 throw new ArgumentException("Carga Horária Inválida");
@@ -105,15 +89,20 @@ namespace TreimanetoTDD.Dominio.Testes.Cursos
             if (valor < 0)
                 throw new ArgumentException("Valor Inválido");
 
+            
+
             Nome = nome;
             CargaHoraria = cargaHoraria;
             PublicoAlvo = publicoAlvo;
             Valor = valor;
+            Descricao = descricao;
         }
 
         public string Nome { get; private set; }
         public int CargaHoraria { get; private set; }
         public PublicoAlvo PublicoAlvo { get; private set; }
         public decimal Valor { get; private set; }
+        public string Descricao { get; private set; }
+
     }
 }
